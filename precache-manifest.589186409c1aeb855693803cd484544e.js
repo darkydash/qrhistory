69c1e5b70227b6e7fa21e3038a71fbc6efde4d00
darkydash <darkydashie@gmail.com>
2020-09-03 21:08:29 +0300

    Updates

---
+++ precache-manifest.589186409c1aeb855693803cd484544e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d1f585f5ccd59f6e24e58bddee61e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff1644b2f59abea393ae",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fc0ae201f4f43f9a78d",
+    "url": "/static/js/2.2fb82ef2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2fb82ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1644b2f59abea393ae",
+    "url": "/static/js/main.4a133ae3.chunk.js"
+  },
+  {
+    "revision": "7464eeed0f2a7d7e83ad",
+    "url": "/static/js/runtime-main.023a1763.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
